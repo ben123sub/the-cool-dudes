84521e96079eb6bcbbf7e5744612bdf82468f92e
Mykah Taylor <kahben13@yahoo.com>
2017-02-13 21:16:43 +0000

    we changed things

--- Dudes.js
+++ Dudes.js
@@ -1,11 +1,17 @@
 $(document).ready( function(){
+ 
   $("button").click( function(){
+      
       var animal; 
-      animal="Ancient Psychic War Elephant";
+     animal="Ancient Psychic War Elephant";
       var guess=$("input").val();
       if (animal ==guess ){
-          $("p").html( "Hello");
-      }
+       $("p").html( "eezy peezy livin breezy");
+       $(".hint").hide();      
+        
+      };
       
   });
+
+    
 });
